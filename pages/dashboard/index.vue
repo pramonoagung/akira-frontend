bf350fba1934bd255aacac8070d1789be0dcb004
Agung Pramono <agung.prasatu@gmail.com>
2018-07-04 10:08:44 +0700

    Remove header component

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,15 +1,12 @@
 <template>
     <div>
-        <Header />
         <Navbar />
     </div>
 </template>
 <script>
-import Header from "~/components/Header";
 import Navbar from "~/components/Navbar";
 export default {
   components: {
-    Header,
     Navbar
   }
 };
