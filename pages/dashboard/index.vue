8b09403bf8be3a243ecca949690d7c183e9c26da
Agung Pramono <agung.prasatu@gmail.com>
2018-07-04 09:54:38 +0700

    Add dashboard view

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Header />
+        <Navbar />
+    </div>
+</template>
+<script>
+import Header from "~/components/Header";
+import Navbar from "~/components/Navbar";
+export default {
+  components: {
+    Header,
+    Navbar
+  }
+};
+</script>
