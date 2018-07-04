8300ce76fb1c5fd20f0c1131253ca0f9ce936a77
Agung Pramono <agung.prasatu@gmail.com>
2018-07-04 09:42:21 +0700

    Register the assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,53 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-library',
+    title: "Akira",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt base library' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Akira Dashboard" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900"
+      },
+      { rel: "stylesheet", href: "/css/icons/icomoon/styles.css" },
+      { rel: "stylesheet", href: "/css/bootstrap.css" },
+      { rel: "stylesheet", href: "/css/core.css" },
+      { rel: "stylesheet", href: "/css/components.css" },
+      { rel: "stylesheet", href: "/css/colors.css" }
+    ],
+    script: [
+      { type: "text/javascript", src: "/js/core/libraries/jquery.min.js" },
+      { type: "text/javascript", src: "/js/core/libraries/bootstrap.min.js" },
+      { type: "text/javascript", src: "/js/core/app.js" },
+      { type: "text/javascript", src: "/js/pages/dashboard.js" },
+      {
+        type: "text/javascript",
+        src: "/js/pages/layout_navbar_secondary_fixed.js"
+      },
+      {
+        type: "text/javascript",
+        src: "/js/pages/dashboard.js"
+      },
+      { type: "text/javascript", src: "/js/plugins/loaders/pace.min.js" },
+      {
+        type: "text/javascript",
+        src: "/js/plugins/ui/headroom/headroom.min.js"
+      },
+      { type: "text/javascript", src: "/js/plugins/ui/ripple.min.js" },
+      { type: "text/javascript", src: "/js/plugins/loaders/blockui.min.js" },
+      { type: "text/javascript", src: "/js/plugins/ui/nicescroll.min.js" },
+      { type: "text/javascript", src: "/js/plugins/ui/drilldown.js" }
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,15 +57,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
+};
