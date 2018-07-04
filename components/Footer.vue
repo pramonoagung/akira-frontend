c05365b7142f9b4d088289ee4fd98ecec106538b
Agung Pramono <agung.prasatu@gmail.com>
2018-07-04 09:53:41 +0700

    Add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <!-- Footer -->
+	<div class="footer text-muted text-center">
+		&copy; 2018. <a href="#">Akira</a> by <a href="#" target="_blank">Akira</a>
+	</div>
+	<!-- /footer -->
+</template>
