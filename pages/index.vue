5c8bc5c003742693cf50633641f80b426e80d537
Agung Pramono <agung.prasatu@gmail.com>
2018-07-04 09:43:47 +0700

    Add login view

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,61 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-library
-      </h1>
-      <h2 class="subtitle">
-        Nuxt base library
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div class="login-container">
+    <!-- Page container -->
+    <div class="page-container">
+
+      <!-- Page content -->
+      <div class="page-content">
+
+        <!-- Main content -->
+        <div class="content-wrapper">
+
+          <!-- Simple login form -->
+          <form action="index.html">
+            <div class="panel panel-body login-form">
+              <div class="text-center">
+                <div class="icon-object border-slate-300 text-slate-300">
+                  <i class="icon-reading"></i>
+                </div>
+                <h5 class="content-group">Login to Akira
+                  <small class="display-block">Masuk ke Akira Dashboard</small>
+                </h5>
+              </div>
+
+              <div class="form-group has-feedback has-feedback-left">
+                <input type="text" class="form-control" placeholder="Username">
+                <div class="form-control-feedback">
+                  <i class="icon-user text-muted"></i>
+                </div>
+              </div>
+
+              <div class="form-group has-feedback has-feedback-left">
+                <input type="password" class="form-control" placeholder="Password">
+                <div class="form-control-feedback">
+                  <i class="icon-lock2 text-muted"></i>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <nuxt-link :to="'/dashboard'">
+                  <button type="submit" class="btn bg-info btn-block">Login
+                </button>
+                </nuxt-link>
+              </div>
+
+              <div class="text-center">
+                <a href="login_password_recover.html">Forgot password?</a>
+              </div>
+            </div>
+          </form>
+          <!-- /simple login form -->
+
+        </div>
+        <!-- /main content -->
+
       </div>
+      <!-- /page content -->
+
     </div>
-  </section>
+    <!-- /page container -->
+  </div>
 </template>
-
-<script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
