d1b65ea65e54fe2fc48cff41520972553450a1a6
Agung Pramono <agung.prasatu@gmail.com>
2018-07-05 22:09:23 +0700

    Add transaksi view

---
+++ pages/dashboard/aplikasi/laporan/transaksi/index.vue
@@ -0,0 +1,94 @@
+<template>
+<div>
+    <!-- Page container -->
+    <div class="page-container">
+
+        <!-- Page content -->
+        <div class="page-content">
+
+            <!-- Main content -->
+            <div class="content-wrapper">
+
+                <!-- Basic datatable -->
+                <div class="panel panel-flat">
+                    <div class="panel-heading">
+                        <h5 class="panel-title">Daftar Transaksi</h5>
+                    </div>
+                    <table class="table datatable-basic table-hover">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>ID</th>
+                                <th>lorem</th>
+                                <th>Status Akun</th>
+                                <th class="text-center">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>
+                                    KIGTGF
+                                </td>
+                                <td>lorem</td>
+                                <td>
+                                    <span class="label label-success">Sukses</span>
+                                </td>
+                                <td class="text-center">
+                                    <ul class="icons-list">
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                                <i class="icon-menu9"></i>
+                                            </a>
+
+                                            <ul class="dropdown-menu dropdown-menu-right">
+                                                <li>
+                                                    <nuxt-link to="/dashboard/aplikasi/manajemen/admin/1/edit">
+                                                        <i class="icon-pencil"></i> Edit
+                                                    </nuxt-link>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>
+                                    HGDFDJ
+                                </td>
+                                <td>lorem</td>
+                                <td>
+                                    <span class="label label-default">Batal</span>
+                                </td>
+                                <td class="text-center">
+                                    <ul class="icons-list">
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                                <i class="icon-menu9"></i>
+                                            </a>
+
+                                            <ul class="dropdown-menu dropdown-menu-right">
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="icon-pencil"></i> Edit</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /basic datatable -->
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+  layout: "dashboard"
+};
+</script>
