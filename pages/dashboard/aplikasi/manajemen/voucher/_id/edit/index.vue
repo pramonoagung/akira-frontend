cfbb77149218d8b867c8c13add4678f1d3f87714
Agung Pramono <agung.prasatu@gmail.com>
2018-07-05 21:57:17 +0700

    Add voucher list view

---
+++ pages/dashboard/aplikasi/manajemen/voucher/_id/edit/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <!-- Page container -->
+    <div class="page-container">
+
+        <!-- Page content -->
+        <div class="page-content">
+
+            <!-- Main content -->
+            <div class="content-wrapper">
+
+                <h2>Edit voucher ID : <b>{{$route.params.id}}</b></h2>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+  layout: "dashboard"
+};
+</script>
---
+++ pages/dashboard/aplikasi/manajemen/voucher/index.vue
@@ -0,0 +1,97 @@
+<template>
+<div>
+    <!-- Page container -->
+    <div class="page-container">
+
+        <!-- Page content -->
+        <div class="page-content">
+
+            <!-- Main content -->
+            <div class="content-wrapper">
+
+                <!-- Basic datatable -->
+                <div class="panel panel-flat">
+                    <div class="panel-heading">
+                        <h5 class="panel-title">Daftar Admin</h5>
+                    </div>
+                    <table class="table datatable-basic table-hover">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Kode</th>
+                                <th>Nilai</th>
+                                <th>Kuota Tersedia</th>
+                                <th>Status Akun</th>
+                                <th class="text-center">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>
+                                    WKWK
+                                </td>
+                                <td>Rp 2500</td>
+                                <td>25</td>
+                                <td>
+                                    <span class="label label-success">Aktif</span>
+                                </td>
+                                <td class="text-center">
+                                    <ul class="icons-list">
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                                <i class="icon-menu9"></i>
+                                            </a>
+
+                                            <ul class="dropdown-menu dropdown-menu-right">
+                                                <li>
+                                                    <nuxt-link to="/dashboard/aplikasi/manajemen/voucher/1/edit">
+                                                        <i class="icon-pencil"></i> Edit
+                                                    </nuxt-link>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>
+                                    HAHA
+                                </td>
+                                <td>Rp 10000</td>
+                                <td>0</td>
+                                <td>
+                                    <span class="label label-default">Kadaluarsa</span>
+                                </td>
+                                <td class="text-center">
+                                    <ul class="icons-list">
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                                <i class="icon-menu9"></i>
+                                            </a>
+
+                                            <ul class="dropdown-menu dropdown-menu-right">
+                                                <li>
+                                                    <a href="#">
+                                                        <i class="icon-pencil"></i> Edit</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /basic datatable -->
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+  layout: "dashboard"
+};
+</script>
