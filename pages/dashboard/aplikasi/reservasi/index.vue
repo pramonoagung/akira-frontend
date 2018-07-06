9eeb3d2a52f19e195eaaf3877d254f0100ab4c1f
Agung Pramono <agung.prasatu@gmail.com>
2018-07-06 10:22:48 +0700

    Add kalender harian

---
+++ pages/dashboard/aplikasi/reservasi/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="page-container">
+		<div class="page-content">			
+			<div class="content-wrapper">
+
+				<!-- Agenda view -->
+				<div class="panel panel-flat">
+					<div class="panel-heading">
+						<h5 class="panel-title">Agenda</h5>
+					</div>
+
+					<div class="panel-body">
+						<p class="content-group">Agenda terapis dalam satu bulan</p>
+
+						<div class="fullcalendar-agenda"></div>
+					</div>
+				</div>
+				<!-- agenda view -->
+
+			</div>
+			<!-- /main content -->
+
+		</div>
+		<!-- /page content -->
+
+	</div>
+	<!-- /page container -->
+	
+</template>
+<script>
+export default {
+  layout: "dashboard"
+};
+</script>
